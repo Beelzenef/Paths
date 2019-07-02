4f6b5ae57b9ca092944cf897a51e67baa5f7f8e4
Elena G <elena.guzbla@gmail.com>
2019-07-02 21:33:35 +0200

    Hide actions, set Item as button

--- Paths/scripts/item.gd
+++ Paths/scripts/item.gd
@@ -1,13 +1,19 @@
-extends Node2D
+extends Button
 
 onready var content = get_node("Content")
 onready var editContent = get_node("Actions/EditContent")
+onready var actionsPanel = get_node("Actions")
+
+func _on_Item_pressed():
+	actionsPanel.visible = not actionsPanel.visible
 
 # actions
 
 func _on_EditButton_pressed():
 	if (editContent.visible):
 		content.text = editContent.text
+	else:
+		editContent.text = content.text
 	editContent.visible = not editContent.visible
 
 func _on_ClearButton_pressed():
