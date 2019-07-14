c6cd657df77e8481fae3d483e1967bb837b9fb30
Elena G <elena.guzbla@gmail.com>
2019-07-14 18:34:38 +0200

    Manage show/hide actions in ItemSet
    Selecting an Item shows the actions within. If any other Item is selected, the last Item selected hides the actions and current are shown.
    
    Also fix adding 4+ Items

--- Paths/scripts/item.gd
+++ Paths/scripts/item.gd
@@ -4,8 +4,15 @@ onready var content = get_node("Content")
 onready var editContent = get_node("Actions/EditContent")
 onready var actionsPanel = get_node("Actions")
 
+signal selected_item
+
+func _ready():
+	connect("selected_item", get_parent(), "manage_selected_item", [self])
+
 func _on_HideActions_Button_pressed():
-	print("hide actions")
+	emit_signal("selected_item")
+
+func hide_actions():
 	actionsPanel.visible = not actionsPanel.visible
 
 # actions
--- Paths/scripts/itemSet.gd
+++ Paths/scripts/itemSet.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 onready var itemScene = preload("res://scenes/items/item.tscn")
 
-onready var SLOT1 = get_node("Slots/Slot1")
-onready var SLOT2 = get_node("Slots/Slot2")
-onready var SLOT3 = get_node("Slots/Slot3")
-onready var SLOT4 = get_node("Slots/Slot4")
+onready var SLOT1 = get_node("Slot1")
+onready var SLOT2 = get_node("Slot2")
+onready var SLOT3 = get_node("Slot3")
+onready var SLOT4 = get_node("Slot4")
+
+var selected_item
 
 func ready():
 	pass
@@ -13,11 +15,12 @@ func ready():
 func _on_AddItem_pressed():
 	var slot = chooseSlot()
 	
-	var newItem = itemScene.instance()
-	newItem.position.x = slot.get_global_position().x
-	newItem.position.y = slot.get_global_position().y
-	slot.fullSlot()
-	add_child(newItem)
+	if (slot):
+		var newItem = itemScene.instance()
+		newItem.position.x = slot.get_global_position().x
+		newItem.position.y = slot.get_global_position().y
+		slot.fullSlot()
+		add_child(newItem)		
 
 func chooseSlot():
 	if (not SLOT1.isFull):
@@ -28,3 +31,9 @@ func chooseSlot():
 		return SLOT3
 	if (not SLOT4.isFull):
 		return SLOT4
+
+func manage_selected_item(item):
+	if (selected_item and selected_item != item):
+		selected_item.hide_actions()
+	item.hide_actions()
+	selected_item = item
